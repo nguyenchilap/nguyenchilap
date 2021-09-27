8ff67077b188ddc72c69736e27a46cf224b57681
LẬP NGUYỄN CHÍ <18120435@student.hcmus.edu.vn>
2021-09-27 18:43:46 +0700

    updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,14 +6,19 @@ const $$ = document.querySelectorAll.bind(document);
 var curTabIndex = 1;
 
 const tabSections = $$('.filter__item');
+const years = $$('.grade__title');
 
 console.log(tabSections);
 
 tabSections.forEach((tabSection, index) => {   
     tabSection.onclick = function(){
+        //remove
         $('.filter__grade.filter__item--active').classList.remove('filter__item--active');
-        $('.grade__body.grade__body--active')?.classList.remove('grade__body--active');
-
+        $('.grade__body.grade__body--active').classList.remove('grade__body--active');
+        $('.grade__title.container__title--active').classList.remove('container__title--active')
+        
+        //add
+        years[index].classList.add('container__title--active');
         $(`.grade-${index+1}-1`)?.classList.add('grade__body--active');
         this.classList.add('filter__item--active');
 
